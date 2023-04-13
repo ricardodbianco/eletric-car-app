a5a0ac5d98a3b0f9a00e503ef79f68c2efb8ae76
ricardodbianco <ricardo.bianco98@hotmail.com>
2023-04-13 19:34:07 -0300

    Aberto nova activity

---
+++ app/src/main/java/com/example/eletricarapp/presentation/CalcularAutonomiaActivity.kt
@@ -0,0 +1,15 @@
+package com.example.eletricarapp.presentation
+
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.eletricarapp.R
+
+class CalcularAutonomiaActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_calcular_autonomia)
+    }
+
+}
--- app/src/main/java/com/example/eletricarapp/presentation/MainActivity.kt
+++ app/src/main/java/com/example/eletricarapp/presentation/MainActivity.kt
@@ -1,11 +1,12 @@
-package com.example.eletricarapp
+package com.example.eletricarapp.presentation
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
+import com.example.eletricarapp.R
 
 class MainActivity : AppCompatActivity() {
     lateinit var preco: EditText
@@ -21,15 +22,17 @@ class MainActivity : AppCompatActivity() {
     }
 
     fun setupView() {
-        preco = findViewById(R.id.et_preco_kwh)
+        //preco = findViewById(R.id.et_preco_kwh)
+        //btnCalcular = findViewById(R.id.btn_calcular)
+        //kmPercorrido = findViewById(R.id.et_km_percorrido)
+        //resultado = findViewById(R.id.tv_resultado)
         btnCalcular = findViewById(R.id.btn_calcular)
-        kmPercorrido = findViewById(R.id.et_km_percorrido)
-        resultado = findViewById(R.id.tv_resultado)
     }
 
     fun setupListeners() {
         btnCalcular.setOnClickListener {
-            calcular()
+            //calcular()
+            startActivity(Intent(this, CalcularAutonomiaActivity::class.java))
 
         }
     }
@@ -53,3 +56,13 @@ class MainActivity : AppCompatActivity() {
 /* Log.d("Resultado -> ", resultado.toString())
     Log.d("Preço -> ", preco.toString())
     Log.d("Km Percorrido -> ", km.toString())*/
+
+/*O Android possui 4 pilares de componentes de aplicativo:
+ 1- Activitys (atividades): ponto de entrada para interação de usuário.
+  Ela representa uma tela única com a interface do usuário.
+ 2- Serviços: serviço em Android é um ponto de entrada para manter um
+  aplicativo em execução em segundo plano.
+ 3- Broadcast Receiver: é um componente que faz o sistema entregar eventos
+  ao aplicativo fora do fluxo do usuário comum.
+ 4- Provider (Provedor de Conteúdo): gerenciam um conjunto de dados compartilhados do
+   aplicativo que você pode armazenar em sistemas de arquivos. */
